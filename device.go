29f901b2c6db502e19ba9f565813ee5c6ac36c20
Gnaphron <guillaume.goussard@gmail.com>
2021-07-01 19:19:46 -0700

    Expose Device data

--- device.go
+++ device.go
@@ -8,19 +8,19 @@ import (
 // Device represents a loop device /dev/loop#
 type Device struct {
 	// device number (i.e. 7 if /dev/loop7)
-	number uint64
+	Number uint64
 
 	// flags with which to open the device with
-	flags int
+	Flags int
 }
 
 // open returns a file handle to /dev/loop# and returns an error if it cannot
 // be opened.
 func (device Device) open() (*os.File, error) {
-	return os.OpenFile(device.Path(), device.flags, 0660)
+	return os.OpenFile(device.Path(), device.Flags, 0660)
 }
 
 // Path returns the path to the loopback device
 func (device Device) Path() string {
-	return fmt.Sprintf(DeviceFormatString, device.number)
+	return fmt.Sprintf(DeviceFormatString, device.Number)
 }
